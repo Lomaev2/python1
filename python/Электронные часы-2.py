59482864e6b5da5d56d3a24462dcd10d70d3ae7e
Lomaev2 <52202622+Lomaev2@users.noreply.github.com>
2019-06-26 15:17:31 +0300

    Add files via upload

--- python/Электронные часы-2.py
+++ python/Электронные часы-2.py
@@ -1,5 +1,18 @@
-n = int(input())
-minuts = n//60
-chas=minuts//60 #Час
-mi=(n//60)%60 #Минуты
-#print(chas,':',mi,':',sec, sep='')
+N = int(input())
+
+minutes = N//60
+hours = 0
+
+if minutes >= 60:
+    hours = minutes//60
+    minutes -= hours*60
+
+seconds = N - (hours*60*60 + minutes*60)
+
+if minutes < 10:
+    if seconds < 10:
+        print(hours, ':', '0', minutes, ':', '0', seconds, sep='')
+    else:
+        print(hours, ':', '0', minutes, ':', seconds, sep='')
+else:
+    print(hours, ':', minutes, ':', seconds, sep='')
